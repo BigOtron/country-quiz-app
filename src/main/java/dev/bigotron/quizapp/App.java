a0276bb5bc6c1523a639b204ba207f5ad83c8c9c
Muhammad Karimov <karimov.shermukhammad.tech@gmail.com>
2025-07-29 11:29:02 +0500

    initialize project: added .gitignore and pom.xml for spring-boot setup

---
+++ src/main/java/dev/bigotron/quizapp/App.java
@@ -0,0 +1,13 @@
+package dev.bigotron.quizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
